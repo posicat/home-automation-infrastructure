1edc7893689a3a2e57a4f05ab75af625e736da00
posicat@gmail.com <posicat@gmail.com@02273557-32ad-90d5-80b9-7f6e01bfe8cd>
2014-10-26 15:20:52 +0000

    Initial import of current spike into functionality.  No testing YET, that's what I plan on setting up next.

---
+++ HomeAutomationHub/src/hub/ConnectionManager.java
@@ -0,0 +1,62 @@
+package hub;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+
+public class ConnectionManager implements Runnable {
+
+	protected int serverPort;
+	protected ServerSocket serverSocket = null;
+	protected boolean isStopped = false;
+	protected Thread runningThread = null;
+	protected ChannelController controller = new ChannelController();
+
+	public ConnectionManager(int port) {
+		this.serverPort = port;
+	}
+
+	public void run() {
+		synchronized (this) {
+			this.runningThread = Thread.currentThread();
+		}
+		openServerSocket();
+		while (!isStopped()) {
+			Socket clientSocket = null;
+			try {
+				clientSocket = this.serverSocket.accept();
+			} catch (IOException e) {
+				if (isStopped()) {
+					System.out.println("Server Stopped.");
+					return;
+				}
+				throw new RuntimeException("Error accepting client connection",	e);
+			}
+			new Thread(new NodeController(clientSocket, NodeController.MODE_PERSISTENT_SOCKET, controller)).start();
+		}
+		System.out.println("Server Stopped.");
+		this.stop();
+	}
+
+	synchronized boolean isStopped() {
+		return this.isStopped;
+	}
+
+	public synchronized void stop() {
+		this.isStopped = true;
+		try {
+			this.serverSocket.close();
+		} catch (IOException e) {
+			throw new RuntimeException("Error closing server", e);
+		}
+	}
+
+	private void openServerSocket() {
+		try {
+			this.serverSocket = new ServerSocket(this.serverPort);
+		} catch (IOException e) {
+			throw new RuntimeException("Cannot open port 8080", e);
+		}
+	}
+
+}
